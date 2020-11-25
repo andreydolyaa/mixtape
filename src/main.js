3ecfa0295a8c7e140f5fd69cecf607b94000650e
Eilon Shetrit <eilonshetrit@gmail.com>
2020-11-25 14:45:29 +0200

    login/signup second commit

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store/store.js'
 import './styles/styles.scss'
-
+import elementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
-
+Vue.use(elementUI);
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
