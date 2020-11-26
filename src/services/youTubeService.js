c4f564a83fd4ea6b7a1339312f2de5a374168cf8
Andrey Dolya <dolya7kk@gmail.com>
2020-11-26 16:17:37 +0200

    sdf

--- src/services/youTubeService.js
+++ src/services/youTubeService.js
@@ -7,7 +7,6 @@ const API_KEY = 'AIzaSyCy6hOuYH-4WoOK2wfJ14CVE1U8HW6Dp70';
 
 export const youTubeService = {
     query,
-    // loadSongDetails
 }
 
 
@@ -18,7 +17,7 @@ async function query(keyword){
     var songId = res.data.items[0].id.videoId
     var songDetails = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?maxResults=1&part=snippet&&id=${songId}&key=${API_KEY}`);
     songDetails.data.items[0].snippet.songId = songId;
-    console.log('@@ song details ',songDetails.data.items[0].snippet);
+    console.log('@@ song details + id - api call result: ',songDetails.data.items[0].snippet);
     return songDetails.data.items[0].snippet;
 }
 
