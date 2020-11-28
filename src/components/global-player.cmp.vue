c0765482e7aaf631f2670e90c1b38e573dd3161e
Andrey Dolya <dolya7kk@gmail.com>
2020-11-28 16:03:25 +0200

    sdfsd

--- src/components/global-player.cmp.vue
+++ src/components/global-player.cmp.vue
@@ -2,39 +2,52 @@
 
 
 <template>
-	<section class='globalPlayer'>
+	<section class="globalPlayer">
 		<div>
-			<div v-if='getCurrSongPlaying'>
-				<youtube :video-id='getCurrSongPlaying.songUrlId' ref='youtube' @playing='playing' :player-vars='playerVars' class='youtube-window'></youtube>
+			<div v-if="getCurrSongPlaying">
+				<youtube :video-id="getCurrSongPlaying.songUrlId" ref="youtube" @playing="playing" :player-vars="playerVars" class="youtube-window" @ended="ended"></youtube>
 			</div>
 		</div>
-		<div class='global-player'>
-			<p><i class="fas fa-music"></i></p>
-			<h2 v-if='getCurrSongPlaying'>{{getCurrSongPlaying.title}}</h2>
+		<div class="global-player">
+			<p>
+				<i class="fas fa-music"></i>
+			</p>
+			<h2 v-if="getCurrSongPlaying">{{getCurrSongPlaying.title}}</h2>
 			<h2 v-else>Artist - Unknown</h2>
-			<button v-if='!isPlayingNow' @click='play'>
-				<i class='far fa-play-circle'></i>
+			<button v-if="!isPlayingNow" @click="play">
+				<i class="far fa-play-circle"></i>
 			</button>
-			<button v-else @click='pause'>
-				<i class='far fa-pause-circle'></i>
+			<button v-else @click="pause">
+				<i class="far fa-pause-circle"></i>
 			</button>
+			<div>
+				<p v-if="currTime">{{currTime}}</p>
+				<p v-else>00:00</p>
+				<input @input="moveTo" type="range" :min="currTime" :max="totalTime" v-model="moveTime">
+				<p>{{totalTime}}</p>
+			</div>
 		</div>
 	</section>
 </template>
 
 <script>
-import { eventBus } from '@/main.js';
+import { eventBus } from "@/main.js";
+import {utilService} from "@/services/utilService.js";
 export default {
-	name: 'global-player',
+	name: "global-player",
 	data() {
 		return {
-			songId: '',
+			songId: "",
 			isPlaying: false,
 			isPause: false,
 			playerVars: {
 				autoplay: 1,
-				origin: window.location.origin
+				origin: window.location.origin,
 			},
+			currTime:'',
+			totalTime:'',
+			moveTime:'',
+			newTime:''
 		};
 	},
 	computed: {
@@ -47,29 +60,71 @@ export default {
 		isPlayingNow() {
 			return this.$store.getters.getCurrSongIsPlaying;
 		},
+		currMix() {
+			return this.$store.getters.getMix;
+		},
+	},
+	watch: {
+
 	},
 	methods: {
 		pause() {
 			this.$refs.youtube.player.pauseVideo();
 			this.$store.commit({
-				type: 'stopSongPlaying',
+				type: "stopSongPlaying",
 			});
 		},
 		play() {
 			this.$refs.youtube.player.playVideo();
 			this.$store.commit({
-				type: 'startSongPlaying',
+				type: "startSongPlaying",
 			});
 		},
-		playing() {
-
+		playing(event) {
+			this.totalTime = utilService.convertSecondsToTime(Math.floor(event.getDuration()));
+			setInterval(()=>{
+				this.currTime = utilService.convertSecondsToTime(Math.floor(event.getCurrentTime()))
+			},1000)
+			this.moveTo(event)
 		},
+		change(){
+			
+		},
+		ended() {
+			this.playNextSong();
+		},
+		playNextSong() {
+			var mixLength = this.currMix.songs.length;
+			var nextSong;
+			var songId = this.getCurrSongPlaying.songUrlId;
+			const idx = this.currMix.songs.findIndex((song) => song.songUrlId === songId);
+			if (idx < this.currMix.songs.length - 1){
+				nextSong = this.currMix.songs[idx + 1];
+			}
+			else if(idx  === this.currMix.songs.length -1){
+				nextSong = this.currMix.songs[0];
+			}
+			var nextSongId = nextSong.songUrlId;
+			this.$refs.youtube.player.loadVideoById(nextSongId);
+			this.$store.commit({
+				type: "setCurrSong",
+				song: nextSong,
+			});
+		},
+		moveTo(event){
+			console.log('time To move to: ',this.moveTime);
+			console.log('move toEVENT: ',event)
+			if(this.moveTime){
+				this.$refs.youtube.player.seekTo(this.moveTime);
+				// this.moveTime = '';
+			}
+		}
 	},
 	created() {
-		eventBus.$on('pause-music', () => {
+		eventBus.$on("pause-music", () => {
 			this.pause();
 		});
-		eventBus.$on('resume-music', () => {
+		eventBus.$on("resume-music", () => {
 			this.play();
 		});
 	},
@@ -81,3 +136,5 @@ export default {
 
 
 // VLE6Y1q13qE
+//getCurrentTime
+//getDuration
---
+++ src/services/utilService.js
@@ -0,0 +1,20 @@
+
+
+
+export const utilService = {
+    convertSecondsToTime
+}
+
+
+function convertSecondsToTime(seconds) {
+    var minutes = 0, hours = 0;
+    if (seconds / 60 > 0) {
+        minutes = parseInt(seconds / 60, 10);
+        seconds = seconds % 60;
+    }
+    if (minutes / 60 > 0) {
+        hours = parseInt(minutes / 60, 10);
+        minutes = minutes % 60;
+    }
+    return ('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2);
+}
