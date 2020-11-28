1ea873925faaf01a67d8405a3985341edefd7071
harelwn <harelwn@users.noreply.github.com>
2020-11-28 19:50:10 +0200

    v01

--- src/components/mix-song-list.cmp.vue
+++ src/components/mix-song-list.cmp.vue
@@ -1,28 +1,34 @@
 <template>
 	<section class="songs-list">
+
+		<!-- <draggable v-model="myArray" group="people" @start="drag=true" @end="drag=false">
+   				<div v-for="element in myArray" :key="element.id">{{element.name}}</div>
+		</draggable> -->
 		<ul>
-			<li class="songs-details-main flex" v-for="(song,index) in songs" :key="song.id">
-				<div class="songs-details">
-					<button v-if="!song.isPlaying" @click="setCurrSongPlaying(song);startSongPlaying(song,songs);">
-						<i class="far fa-play-circle"></i>
-					</button>
-					<button v-if="song.isPlaying" @click="pauseSong(song);">
-						<i class="far fa-pause-circle"></i>
-					</button>
+			<draggable v-model="songsCopy" group="people" @start="drag=true" @end="drag=false" @end="onEnd">
+				<li class="songs-details-main flex" v-for="(song,index) in songsCopy" :key="song.id">
+					<div class="songs-details">
+						<button v-if="!song.isPlaying" @click="setCurrSongPlaying(song);startSongPlaying(song,songs);">
+							<i class="far fa-play-circle"></i>
+						</button>
+						<button v-if="song.isPlaying" @click="pauseSong(song);">
+							<i class="far fa-pause-circle"></i>
+						</button>
 
 
-					<img :src="song.imgUrl" />
-					<p>{{ song.title }}</p>
-					<span>{{ song.duration }}</span>
-				</div>
-				<div class="sort-songs-buttons">
-					<button v-on:click="emitSongPos(index,-1)"><i class="fas fa-sort-up"></i></button>
-					<button v-on:click="emitSongPos(index,1)"><i class="fas fa-sort-down"></i></button>
-				</div>
-				<span class="delete-song" @click="emitSongId(song.id)">
-					<i class="far fa-trash-alt"></i>
-				</span>
-			</li>
+						<img :src="song.imgUrl" />
+						<p>{{ song.title }}</p>
+						<span>{{ song.duration }}</span>
+					</div>
+					<div class="sort-songs-buttons">
+						<button v-on:click="emitSongPos(index,-1)"><i class="fas fa-sort-up"></i></button>
+						<button v-on:click="emitSongPos(index,1)"><i class="fas fa-sort-down"></i></button>
+					</div>
+					<span class="delete-song" @click="emitSongId(song.id)">
+						<i class="far fa-trash-alt"></i>
+					</span>
+				</li>
+			</draggable>
 		</ul>
 		
 	</section>
@@ -31,6 +37,7 @@
 <script>
 import { mixService } from "@/services/mixService.js";
 import {eventBus} from '@/main.js';
+import draggable from 'vuedraggable'
 
 export default {
 	name: "mix-song-list",
@@ -47,6 +54,7 @@ export default {
 				//origin: window.location.origin, // or http(S)://your.domain.com
 				origin:'http://localhost:8080/'
 			},
+			songsCopy: JSON.parse(JSON.stringify(this.songs)),
 		};
 	},
 	computed: {
@@ -58,6 +66,9 @@ export default {
 		},
 	},
 	methods: {
+		onEnd(){
+			console.log('drag end');
+		},
 		emitSongPos(songIdx,diff) {
 			this.$emit("emitSongPos", {songIdx:songIdx,diff:diff});
 		},	
@@ -100,7 +111,7 @@ export default {
 
 	},
 	components:{
-		
+		draggable
 	},
 }
 </script>
