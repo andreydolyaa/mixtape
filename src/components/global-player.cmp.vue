a5000c6a04b78650d378bcced43984233cc9cb59
Andrey Dolya <dolya7kk@gmail.com>
2020-11-28 19:21:44 +0200

    sdfsd

--- src/components/global-player.cmp.vue
+++ src/components/global-player.cmp.vue
@@ -33,6 +33,10 @@
 				<button @click="onNextSong">NEXT</button>
 				<button @click="onPrevSong">PREV</button>
 			</div>
+			<div>
+				<button v-if="!isMuted" @click="mute"><i class="fas fa-volume-up"></i></button>
+				<button v-else @click="unmute"><i class="fas fa-volume-mute"></i></button>
+			</div>
 		</div>
 	</section>
 </template>
@@ -55,6 +59,7 @@ export default {
 			currTimePlaying: 0,
 			totalTime: 0,
 			totalTimeInput: 0,
+			isMuted:false
 		};
 	},
 	computed: {
@@ -159,6 +164,14 @@ export default {
 			})
 			
 		},
+		mute(){
+			this.isMuted = true;
+			this.$refs.youtube.player.mute();
+		},
+		unmute(){
+			this.isMuted = false;
+			this.$refs.youtube.player.unMute();
+		}
 	},
 	created() {
 		eventBus.$on("pause-music", () => {
