ba7b0583ef6ec388d8351605b8e06add6eddb019
harelwn <harelwn@users.noreply.github.com>
2020-11-29 20:12:15 +0200

    v01

--- src/components/mix-list-home.cmp.vue
+++ src/components/mix-list-home.cmp.vue
@@ -1,6 +1,7 @@
 <template>
 	<section ref="mix-list-home" class="mix-list-home container">
 		<!-- <h2>Home mix list </h2> -->
+    <!-- mark -->
 		<div ref="mix-list-home container">      
 			<nav class="mixes-nav">
 				<div class="genre-title">{{ genre }}</div>
