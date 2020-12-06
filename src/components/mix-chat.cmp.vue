1f528c9fddaf2e1e92cd882f6b18856436966c9d
Andrey Dolya <dolya7kk@gmail.com>
2020-12-06 18:35:53 +0200

    SDfsdf

--- src/components/mix-chat.cmp.vue
+++ src/components/mix-chat.cmp.vue
@@ -19,8 +19,14 @@
 				</li>
                 
 				<li v-for="(mesg, idx) in msgs" :key="idx">
-					<p class="msg">{{ mesg.name }}: {{ mesg.txt }}</p>
+					<p v-if="mesg.txt" class="msg">{{ mesg.name }}: {{ mesg.txt }}</p>
+                    <div v-if="mesg.gif">
+                        {{mesg.name}}:
+                        <img :src="mesg.gif" class="gif">
+                    </div>
 				</li>
+
+                
 			</ul>
 		</div>
 		<div class="chat-form raised">
@@ -32,7 +38,13 @@
                     <option value="" selected disabled hidden>{{chatEmojis[1]}}</option>
                     <option v-for="emoji in chatEmojis" :value="emoji" :key="emoji">{{emoji}}</option>
                 </select>
+            <select name="gifs" v-model="msg.gif" @change="sendMsg">
+                <option value="" selected disabled hidden>GIF</option>
+                    <option v-for="gif in gifs" :key="gif" :value="gif">gif</option>
+            </select>
+            <button @click="clearChat"><i class="far fa-trash-alt"></i></button>
 				<!-- <button><img src="https://res.cloudinary.com/hw-projects/image/upload/v1606416732/appmixes/send-messege_s_red.png"></button> -->
+                
 			</form>
 		</div>
 	</section>
@@ -48,13 +60,17 @@ export default {
             msg: {
                 name:this.getUser,
                 txt:'',
+                gif:'',
             },
             msgsHistory:[],
             msgs: [],
+            gifsList:[],
+            gif:'',
             room:this.mixId,
             isTyping:false,
             chatEmojis:['🤙','😎','👍','😂','👻','🕺','💃','🤩','🥳','👽','🤖'],
-            users:null
+            users:null,
+            gifs:["https://www.icegif.com/wp-content/uploads/dancing-icegif-3.gif",'https://media0.giphy.com/media/Qz5jqYCH9l5t4Dz1Di/giphy.gif','https://gif-free.com/uploads/posts/2017-11/1509959847_snoopy-dancing.gif','https://www.askideas.com/media/08/Funny-Dancing-Gif-Image.gif','https://media4.giphy.com/media/e7QR593l1Vl94uqs9x/source.gif','https://media4.giphy.com/media/l3V0pGcu4HosNs57a/source.gif','https://media1.giphy.com/media/4QFzDdeLmo19MTm9AF/source.gif','https://i.pinimg.com/originals/5a/c5/50/5ac550d16dc31f593fd3b316eaf47d42.gif']
         };
     },
     computed:{
@@ -70,10 +86,11 @@ export default {
             this.$store.dispatch({ type: "sendMsgWhatsApp"});
         },
         sendMsg(){
-            if(!this.msg.txt) return;
+            // if(!this.msg.txt || !this.msg.gif) return;
             socketService.emit('send message',{msg:this.msg,roomId:this.room});
             console.log('CHAT MSG SENT : ',this.msg);
             this.msg.txt = '';
+            this.msg.gif = '';
         },
         isTypingNow(){
             socketService.emit('is typing',this.isTyping)
@@ -85,13 +102,18 @@ export default {
         },
         setUserName(){
 
+        },
+        clearChat(){
+            this.msgs = [];
+            this.msgsHistory = [];
+            socketService.emit('clear chat')
         }
     },
     created() {
         // socketService.setup();
         // socketService.emit('join room',this.room);
         socketService.on('chat message',message => {
-            this.msgs.push(message)
+            this.msgs.unshift(message)
         })
         socketService.on('type msg',isTyping => {
                 this.isTyping = true;
@@ -113,6 +135,11 @@ export default {
 
         socketService.on('getCount',totalConnected => {
             this.users = totalConnected;
+        })
+
+        socketService.on('clear-all-chat',()=>{
+            this.msgs = [];
+            this.msgsHistory = [];
         })
 
         
